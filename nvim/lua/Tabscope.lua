bfa2feafdc9804870e52877884fccc50f62750b6
Ramil <fathulislamovrr@gmail.com>
2024-04-18 02:56:52 +0500

    bugfix

--- nvim/lua/Tabscope.lua
+++ nvim/lua/Tabscope.lua
@@ -2,7 +2,7 @@ local function config()
 	require("tabscope").setup({})
 
 	-- To remove tab local buffer, use remove_tab_buffer:
-	vim.r.set("n", "<M-o>", require("tabscope").remove_tab_buffer)
+	vim.keymap.set("n", "<M-o>", require("tabscope").remove_tab_buffer)
 end
 return {
 	'backdround/tabscope.nvim',
