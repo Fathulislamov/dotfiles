8224ddda78b1e1215d77c3b33bdb3c4afef87324
Ramil <fathulislamovrr@gmail.com>
2025-09-21 05:07:04 +0500

    deleted unnecessary folders

--- ags/components/Bar/Title.js
+++
@@ -1,9 +0,0 @@
-const hyprland = await Service.import("hyprland")
-
-export const Title = () => {
-  return (
-    Widget.Label({
-      class_name: "bar_item",
-      label: hyprland.active.client.bind("title"),
-    }))
-}
